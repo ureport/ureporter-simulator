24b5181967c77b00bbffca9e7084759f847d11e2
Jim Barritt <jim@planet-ix.com>
2013-04-24 14:15:15 +0300

    [jmdb] Basic project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ureporter-simulator
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ureporter_simulator/core_test.clj
@@ -0,0 +1,7 @@
+(ns ureporter-simulator.core-test
+  (:use clojure.test
+        ureporter-simulator.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
